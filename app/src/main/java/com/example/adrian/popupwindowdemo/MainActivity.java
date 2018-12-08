18a4de7db2fb61cbd31b894c0e4e85faa90c87df
adrothjp <43218716+adrothjp@users.noreply.github.com>
2018-12-07 21:59:31 -0500

    changing

--- app/src/main/java/com/example/adrian/popupwindowdemo/MainActivity.java
+++ app/src/main/java/com/example/adrian/popupwindowdemo/MainActivity.java
@@ -1,29 +1,64 @@
 package com.example.adrian.popupwindowdemo;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_op;
+    /*
+    button to open up the popup.
+    PopupWindow Widget
+    layout inflater
+    and a relative layout linked to the popwindow.xml
+     */
+    private Button btn;
+    private PopupWindow popupWindow;
+    private LayoutInflater layoutInflater;
+    private RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_op = (Button) findViewById(R.id.btn_open);//link button variable with layout button id
+        //reference button and layout in on create method
+        btn = (Button) findViewById(R.id.btn_open);
+        relativeLayout = (RelativeLayout) findViewById(R.id.Relative);
 
-        btn_op.setOnClickListener(new View.OnClickListener() {
+        //creates the view when the button is pressed
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //create an intent that starts the popup window activity
-                Intent i = new Intent(getApplicationContext(), PopActivity.class);
-                startActivity(i);
+                //initialize the layout inflater
+                layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                //create the new layout for the window
+                ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.popwindow, null);
+
+                //create window, pass layout, width, height and if you can close the window if you touch oustside
+                popupWindow = new PopupWindow(container, 400, 400, true);
+                //places the window at the specific location and gravity
+                popupWindow.showAtLocation(relativeLayout, Gravity.NO_GRAVITY,500,500);
+
+                //this instructions is to close the window if we click outside of it
+                container.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        //dismisses the popupWindow
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
             }
         });
     }
